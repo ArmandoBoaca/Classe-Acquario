966a8b35e1f12d2da70e8f66b4707c8c65b2bac7
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-11 21:09:26 +0200

    Test Method clone() & toStirngInner() of Pesce classe

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args){
         Pesce p1 = new Pesce("Sogliola", "Nero");
         System.out.println(p1.toString());
+        Pesce p2 = p1.clone();
+        System.out.println(p2.toStringInner());
+
     }
 }
