fdec0205b2257dd4a59daf032ad7375003b2d7bb
Armando Boaca <55357189+Armmm17@users.noreply.github.com>
2023-10-11 21:23:15 +0200

    Added test for getter methods

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args){
         Pesce p1 = new Pesce("Sogliola", "Nero");
+        System.out.println(p1.getRazza());
+        System.out.println(p1.getColore());
         System.out.println(p1.toString());
         Pesce p2 = p1.clone();
+        System.out.println(p1.getRazza());
+        System.out.println(p1.getColore());
         System.out.println(p2.toStringInner());
 
     }
